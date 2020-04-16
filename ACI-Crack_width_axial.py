3c1183a56d2d7a7042ed0951c1bd2778af4dd801
T4m3R <tamer.arsayed@gmail.com>
2020-04-16 11:09:03 +0200

    commit 5

--- ACI-Crack_width_axial.py
+++ ACI-Crack_width_axial.py
@@ -1,7 +1,7 @@
 def crack_width_tencrack_calc_title():
     print('CRACK WIDTH CALCULATIONS ACCORDING TO ACI: 224R-018')
     print('')
-    print('Tesnsion cracking')
+    print('Tension cracking')
     print('')
 def crack_width_tencrack_inputs():
     n_st_row = int(input('Secondary tension side rebar number of rows:'))
@@ -46,7 +46,7 @@ n_st_row, sec_ten_rebar_dia, sec_ten_rebar_spacing, n_mt_row, main_ten_rebar_dia
 # total tension rebars in the input breadth
 tot_main_ten_rebar = sec_breadth*n_mt_row/main_ten_rebar_spacing
 #print('Total number of main tension bars = ',tot_main_ten_rebar)
-fct = nom_cover_main_rebar+((n_mt_row/2)*main_ten_rebar_dia)+(((n_mt_row-1)/2)*main_ten_rebar_dia)
+cov_center_main_rebar = nom_cover_main_rebar+((n_mt_row/2)*main_ten_rebar_dia)+(((n_mt_row-1)/2)*main_ten_rebar_dia)
 tot_sec_ten_rebar_area = rebar_area_tot_breadth(sec_ten_rebar_dia, sec_ten_rebar_spacing, sec_breadth, n_st_row)
 tot_main_ten_rebar_area = rebar_area_tot_breadth(main_ten_rebar_dia, main_ten_rebar_spacing, sec_breadth, n_mt_row)
 #print('Secondary tension rebar area =',tot_sec_ten_rebar_area, 'mm2')
@@ -76,14 +76,14 @@ while True:
         continue
 #print('Tolerable crack width = ',cond, 'mm')
 # calculating and checking stresses
-main_ten_force =(n_force*(sec_height/2+eccen-cov_sec_rebar))/(sec_height-fct-cov_sec_rebar)
+main_ten_force =(n_force*(sec_height/2+eccen-cov_sec_rebar))/(sec_height-cov_center_main_rebar-cov_sec_rebar)
 main_ten_Stress_rebar =(main_ten_force*1000/tot_main_ten_rebar_area)
 #print ('Tensile stress on main tension rebar =', main_ten_Stress_rebar, 'N/mm2')
 if main_ten_Stress_rebar <= 0.6*rebar_fy:
     print ('Tensile stress on main tension rebar is less than 0.6*fy - Pass')
 else:
     print ('Compressive stress on main compression rebar is larger than 0.6*fy - Tension Failure')
-sec_ten_force =(n_force*(sec_height/2-eccen-fct))/(sec_height-fct-cov_sec_rebar)
+sec_ten_force =(n_force*(sec_height/2-eccen-cov_center_main_rebar))/(sec_height-cov_center_main_rebar-cov_sec_rebar)
 sec_ten_Stress_rebar =(sec_ten_force*1000/tot_sec_ten_rebar_area)
 #print ('Tensile stress on secondary tension rebar =', sec_ten_Stress_rebar, 'N/mm2')
 if sec_ten_Stress_rebar <= 0.6*rebar_fy:
@@ -92,11 +92,11 @@ else:
     print ('Tensile stress on secondary tension rebar is larger than 0.6*fy - Tension Failure')
 # calculating and checking crack width
 # A area of concrete symmetric with reinforcing steel divided by number of bars
-a_symm =(2*sec_breadth*fct)/tot_main_ten_rebar
+a_symm =(2*sec_breadth*cov_center_main_rebar)/tot_main_ten_rebar
 # crack width in mm
 # 1 N/mm2 = 0.145 KSI (Kilo pound force per square inch)
 # 1 in3 = 16387 mm3
-crack_width = (0.1*main_ten_Stress_rebar*0.145*((fct*a_symm/16387)**(1/3))/1000)*25.4
+crack_width = (0.1*main_ten_Stress_rebar*0.145*((cov_center_main_rebar*a_symm/16387)**(1/3))/1000)*25.4
 #print ('Using equation (4-21), Maximum Crack width =', crack_width, 'mm')
 if crack_width <= cond:
     print ('Maximum Crack width is less than tolerable crack width - Pass')
